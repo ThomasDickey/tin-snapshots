07ebc0224c57dc3626a75fb69cd2131c245ff5aa
Thomas E. Dickey <dickey@invisible-island.net>
1996-07-21 14:47:50 +0000

    snapshot of project "tin", label ctin960721

--- include/patchlev.h
+++ include/patchlev.h
@@ -13,7 +13,7 @@
  */
 
 #define VERSION		"1.3 unoff BETA"
-#define RELEASEDATE	"960720"
+#define RELEASEDATE	"960721"
 
 /* This is NO official release, but a patched version.
  * Including patches from:
